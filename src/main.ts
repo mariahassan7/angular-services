733c12cc104f53c5420957368ea8feba9e450497
Maria Hassan <hassan666maria@gmail.com>
2023-09-25 08:34:48 +0500

    removed --disable-host-check warning

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+console.log = console.warn = console.error = () => {};
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
